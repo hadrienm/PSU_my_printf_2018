f0bf82be4e6a59693d4b38ed70b3d0c65fb30d11
Héros Adrien <Juliettead@localhost.localdomain>
2018-11-18 23:07:15 +0100

    error delete

--- lib/my/another_function.c
+++ lib/my/another_function.c
@@ -11,17 +11,20 @@ int another_function (va_list ap, char *str, int i)
 {
     if (str[i] == '%' && (str[i + 1] == 'd' || str[i + 1] == 'i')) {
         display_integer(ap);
-        i = i + 2;
+        i = i + 1;
     } if (str[i] == '%' && str[i + 1] == 'c') {
         display_char(ap);
-        i = i + 2;
+        i = i + 1;
     } if (str[i] == '%' && str[i + 1] == 's') {
         display_argument_string(ap);
-        i = i + 2;
+        i = i + 1;
     } if (str[i] == '%' && str[i + 1] == 'u') {
         display_int_unsigned(ap);
-        i = i + 2;
-    } else
-        my_putchar(str[i]);
+        i = i + 1;
+    } else {
+        if (str[i + 1] != '%' && str[i + 1] != '\0') {
+            my_putchar(str[i]);
+        } 
+    }
     return (i);
 }
--- lib/my/display_argument_string.c
+++ lib/my/display_argument_string.c
@@ -5,15 +5,18 @@
 ** yes
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
 int display_argument_string(va_list ap)
 {
-    char* test;
+    char* test = malloc(sizeof(char*) * 1000);
     int ju = 0;
 
-    test = va_arg(ap, char *);
-    while (test[ju] != '\0') {
+    test = va_arg(ap, char*);
+
+    while (test[ju] != '\0') { 
         my_putchar(test[ju]);
         ju++;
     }
